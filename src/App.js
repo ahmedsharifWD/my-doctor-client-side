64be2b3f2a354ebe0acbf9b75decea3da95b5eae
Ahmed Sharif <ahmedsharif7605276@gmail.com>
2022-06-06 20:27:16 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+import Navbar from './pages/Shared/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
