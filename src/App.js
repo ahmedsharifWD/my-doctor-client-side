3d010cd5b0234c9a1f5a8b772667c4992d527fd7
Ahmed Sharif <ahmedsharif7605276@gmail.com>
2022-06-08 20:58:37 +0600

    add the banner

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
 import Navbar from './pages/Shared/Navbar';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   );
