52e5458e27734ebd6f96b54d8641ca84d598f11a
Ahmed Sharif <ahmedsharif7605276@gmail.com>
2022-06-13 22:55:15 +0600

    recover fetch with 5000

--- src/pages/AppointmentPage/AvailableAppointment.js
+++ src/pages/AppointmentPage/AvailableAppointment.js
@@ -1,10 +1,36 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service';
+import BookingModal from "./BookingModal";
 
 const AvailableAppointment = ({ date }) => {
+    const [services, setServices] = useState([]);
+
+    // this state send data of treatment to modal
+    const [treatment, setTreatment] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/service')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
     return (
         <div>
-            <h2>Available Service on {format(date, 'PP')} </h2>
+            <h4 className='text-secondary text-xl font-semibold text-center'>Available Service on {format(date, 'PP')} </h4>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-12'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                        setTreatment={setTreatment}
+                    ></Service>)
+                }
+            </div>
+            {treatment && <BookingModal
+                date={date}
+                treatment={treatment}
+                setTreatment={setTreatment}
+            ></BookingModal>}
         </div>
     );
 };
